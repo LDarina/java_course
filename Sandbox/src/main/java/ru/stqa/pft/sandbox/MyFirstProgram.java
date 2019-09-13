9c4e59d6b5dc5ea0844c9005f7fae63e6f6347f9
LDarina <darinelly@mail.ru>
2019-09-13 10:18:13 +0300

    задание 2

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,19 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-System.out.println("Hello, world!");
-	}
+  System.out.println("Hello, world!");
+
+  Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+  Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+		Point p1 = new Point(1, 3);
+    Point p2 = new Point(4,6);
+    System.out.println("Расстояние между точками  = " + p1.distance(p1, p2) );
+  }
+
 }
+
