afc1979e47efaad363bd92fe975101440a4e5bc1
LDarina <darinelly@mail.ru>
2019-09-22 20:02:50 +0300

    Добавлена возможность запускать тесты в хроме и IE

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -3,12 +3,14 @@ package tests;
 import appmanager.ApplicationManager;
 import model.ContactData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class TestBase extends ApplicationManager {
+public class TestBase  {
 
-  protected final ApplicationManager app = new ApplicationManager();
+
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
