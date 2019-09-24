7e45e0327f23849e7badb2c2366f9d8671db215b
LDarina <darinelly@mail.ru>
2019-09-24 15:12:49 +0300

    оптимизированы переходы на страницы

--- addressbook-web-tests/src/test/java/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/appmanager/NavigationHelper.java
@@ -10,13 +10,22 @@ public class NavigationHelper extends HelperBase {
    super(wd);
   }
    public void gotoGroupPage() {
-   click(By.linkText("groups")); }
+     if (isElementPresent(By.tagName("h1"))
+             && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+             && isElementPresent(By.name("new"))) {
+       return;
+     }
+     click(By.linkText("groups"));
+   }
 
   public void gotoAddNew() {
     click(By.linkText("add new"));
   }
 
   public void gotoHome() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
   public void returnToHomePage() {
