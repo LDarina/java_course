1b3c9f3627c7106357f6fa8e36d4a13b312f59a3
LDarina <darinelly@mail.ru>
2019-10-09 11:54:41 +0300

    запуск всех тестов групп в одном окне браузера

--- addressbook-web-tests/src/test/java/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactDeletionTests.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class ContactDeletionTests extends TestBase{
 
-  @Test
+  @Test(enabled = false)
   public void testContactDeletion() {
     app.getNavigationHelper().gotoHome();
     if (! app.getContactHelper().isThereAContact()) {
