11c976cb767f8a006c33985533d5f1c5b0144771
LDarina <darinelly@mail.ru>
2019-11-05 18:21:52 +0300

    добавлен новый модуль для тестов для mantis

--- addressbook-web-tests/src/test/java/tests/ContactToGroupTest.java
+++ addressbook-web-tests/src/test/java/tests/ContactToGroupTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static tests.TestBase.app;
 
-public class ContactToGroupTest {
+public class ContactToGroupTest extends TestBase {
 
   private ContactData chosenContact;
   private GroupData chosenGroup;
